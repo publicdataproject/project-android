4d52881a623f6733bac83873b4c64b0573287db9
seunghee63 <0603yang@naver.com>
2019-06-16 03:18:00 +0900

    add googleMapAPI to shopTab

--- app/src/main/java/com/song2/publicdata_project/Fragment/ShopFragment.kt
+++ app/src/main/java/com/song2/publicdata_project/Fragment/ShopFragment.kt
@@ -1,22 +1,64 @@
 package com.song2.publicdata_project.Fragment
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-
+import android.view.*
 import com.song2.publicdata_project.R
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapsInitializer
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 
-class ShopFragment : Fragment() {
+import com.google.android.gms.maps.OnMapReadyCallback
+
+class ShopFragment : Fragment(),OnMapReadyCallback{
+
+    lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shop, container, false)
+    ): View {
+        val layout = inflater.inflate(R.layout.fragment_shop, container, false)
+        mapView = layout.findViewById(R.id.mv_shop_fragment)
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync(this)
+
+        return layout
     }
 
+    override fun onResume() {
+        mapView.onResume()
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        MapsInitializer.initialize(this.getActivity())
+
+        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng(35.141233, 126.925594), 14f)
+
+        googleMap.animateCamera(cameraUpdate)
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(LatLng(35.141233, 126.925594))
+                .title("루프리코리아")
+        )
+
+    }
 }
