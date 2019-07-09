549e0b98abc36237ae517a617990e797a9b031be
최민정 <laura126@ajou.ac.kr>
2019-07-09 18:14:17 +0900

    merge map,homw,mypage

---
+++ app/src/main/java/com/song2/publicdata_project/Data/ShopListData.kt
@@ -0,0 +1,6 @@
+package com.song2.publicdata_project.Data
+
+data class ShopListData(var shop_id: Int,
+                        var shop_img: String,
+                        var shop_name: String,
+                        var shop_location: String)
--- app/src/main/java/com/song2/publicdata_project/Fragment/ShopFragment.kt
+++ app/src/main/java/com/song2/publicdata_project/Fragment/ShopFragment.kt
@@ -1,21 +1,30 @@
 package com.song2.publicdata_project.Fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.*
+import android.widget.LinearLayout
 import com.song2.publicdata_project.R
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.MapsInitializer
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.song2.publicdata_project.Data.ShopListData
+import com.song2.publicdata_project.adapter.ShopListRecyclerViewAdapter
+import kotlinx.android.synthetic.main.fragment_shop.*
 
 class ShopFragment : Fragment(),OnMapReadyCallback{
 
     lateinit var mapView: MapView
+
+    lateinit var shopListRecyclerViewAdapter : ShopListRecyclerViewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +37,12 @@ class ShopFragment : Fragment(),OnMapReadyCallback{
         mapView.getMapAsync(this)
 
         return layout
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        attachShopListRecyclerView()
     }
 
     override fun onResume() {
@@ -60,5 +75,26 @@ class ShopFragment : Fragment(),OnMapReadyCallback{
                 .title("루프리코리아")
         )
 
+        googleMap.addCircle(
+            CircleOptions()
+                .radius(1000.0)
+                .strokeWidth(0f)
+                .center(LatLng(35.141233, 126.925594))
+                .fillColor(Color.parseColor("#7Fc4c9e8"))
+        )
+
     }
+
+    fun attachShopListRecyclerView(){
+        var dataList : ArrayList<ShopListData> = ArrayList()
+        dataList.add(ShopListData(0,"","군포 농협","군포"))
+        dataList.add(ShopListData(1,"","안양 농협","안양"))
+        dataList.add(ShopListData(2,"","망원 농협","망원"))
+
+        shopListRecyclerViewAdapter= ShopListRecyclerViewAdapter(context!!, dataList)
+        rv_shop_frag_shop_list.adapter = shopListRecyclerViewAdapter
+        rv_shop_frag_shop_list.layoutManager = LinearLayoutManager(context!!, LinearLayout.HORIZONTAL, false)
+    }
+
+
 }
